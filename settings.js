316fa44c1ce2dc62fba642288d0a86604c9307e7
Pete Mertz <peter.mertz@e4sciences.com>
2014-12-15 21:04:38 -0500

    Added settings file

---
+++ settings.js
@@ -0,0 +1,26 @@
+var env = process.node_env;
+
+// Default settings go here
+var settings = {
+    MONGO_URL: 'localhost',	
+    MONGO_DB: 'dev',
+    
+    PORT: 3000,
+
+    LOG_LEVEL: 2,
+}
+
+if ( typeof env == 'undefined' ) {
+    env = 'dev';
+}
+
+if ( env == 'dev' ) {
+
+} else if ( env == 'test' ) {
+    settings.MONGO_DB = 'test';
+    LOG_LEVEL = 1;
+}
+
+//console.log( "Using env:", env );
+
+module.exports = settings;
